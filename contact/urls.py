6e62095c4b052ef91ca019699cf9d10022a70c57
Piotrenewicz <piotrekm9@onet.eu>
2019-07-28 18:26:33 +0200

    Progress on the desing

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "contact"
 urlpatterns = [
+    path('', views.info, name='info'),
     path('map', views.map, name='map'),
     path('map/fullscreen', views.full_map, name='fullmap'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def info(request):
+    return render(request, 'contact/info.html')
+
 def map(request):
     return render(request, 'contact/map.html')
 
