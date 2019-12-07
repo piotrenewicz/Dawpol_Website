a96534b4df8a3d51060a01f5aacfd80822b061f4
Piotrenewicz <piotrekm9@onet.eu>
2019-12-07 17:07:38 +0100

    uslugi and typo fix

--- services/urls.py
+++ services/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = "services"
 urlpatterns = [
-    path('', views.wip, name='wip'),
+    path('', views.service, name='uslugi'),
     # path('details/<int:car_id>', views.detailedview, name='detailed'),
 ]
--- services/views.py
+++ services/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def wip(request):
     return render(request, 'services/wip.html')
+
+
+def service(request):
+    return render(request, 'services/our_services.html')
